0115883afbb23e23c017ca33274d31209aff13a0
Vaughan Jones <vlj7491@gmail.com>
2020-10-17 11:07:32 +1100

    reduce db queries of players scraper job

--- app/jobs/player_scraper_job.rb
+++ app/jobs/player_scraper_job.rb
@@ -5,17 +5,12 @@ class PlayerScraperJob < ApplicationJob
     Team.all.each do |team|
       document = page_data("https://www.nrl.com/players/?team=#{team.nrl_id}", "div#vue-profile-search")
       document['profileGroups'].first['profiles'].each do |profile|
-        player_params = {
+        Player.find_or_create_by({
           first_name: profile['firstName'],
           last_name: profile['lastName'],
-          team_id: Team.find_by(name: profile['teamNickName']).id,
+          team_id: team.id,
           position: profile['position']
-        }
-
-        player = Player.find_by(player_params)
-        if player.nil?
-          Player.create(player_params)
-        end
+        })
       end
     end
   end
