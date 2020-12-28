e756dd8179d636a901b3430ce04c1e659be038bb
Vaughan Jones <vlj7491@gmail.com>
2020-12-29 10:45:55 +1100

    fix some things

--- app/ml_models/result_model.rb
+++ app/ml_models/result_model.rb
@@ -1,6 +1,11 @@
 class ResultModel < Eps::Base
   def build
     games = Game.all
+
+    for game in games do
+      home_game_team = GameTeam.where(game_id: game.id, side: 'home')
+    end
+
     data = games.map { |v| features(v) }
     model = Eps::Model.new(data, target: :result)
     File.write(model_file, model.to_pmml)
@@ -14,17 +19,7 @@ class ResultModel < Eps::Base
   private
 
   def features(game)
-    home_game_team = GameTeam.where(game_id: self.id, side: 'home')
-    home_team = Team.where(id: home_game_team.team_id)
-
-    # todo merge team.stats with prefixed key
-    {
-      home_team_avg_errors_per_game: home_team.avg_errors_per_game,
-      home_team_avg_tries_per_game: home_team.avg_tries_per_game,
-      away_team_avg_errors_per_game: away_team.avg_errors_per_game,
-      away_team_avg_tries_per_game: away_team.avg_tries_per_game,
-      result: game.result
-    }
+    {}
   end
 
   def model
