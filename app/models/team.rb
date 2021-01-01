2b82c5181cb21902b21dd9c4cf70f722f7c0390b
Vaughan Jones <vlj7491@gmail.com>
2021-01-02 10:35:32 +1100

    add more features to model

--- app/models/team.rb
+++ app/models/team.rb
@@ -40,16 +40,67 @@ class Team < ApplicationRecord
     TeamStat.find_by(team_id: self.id, name: 'total_line_breaks').value
   end
 
+  def home_game_wins
+    home_wins = 0
+    gt = GameTeam.where(team_id: self.id, side: 'home')
+
+    for g in gt do
+      game = Game.find(g.game_id)
+      home_wins += 1 if game.result == 'home'
+    end
+
+    home_wins
+  end
+  
+  def away_game_wins
+    away_wins = 0
+    gt = GameTeam.where(team_id: self.id, side: 'away')
+
+    for g in gt do
+      game = Game.find(g.game_id)
+      away_wins += 1 if game.result == 'away'
+    end
+
+    away_wins
+  end
+
+  # TODO: cleanup
   def wins
     wins = 0
     gt = GameTeam.where(team_id: self.id)
 
     for g in gt do
-      game = Game.find_by(game_id: gt.game_id)
-
-      wins += 1 if game.result == gt.side
+      game = Game.find(g.game_id)
+      wins += 1 if game.result == g.side
     end
 
     wins
+  end
+
+  # TODO: cleanup
+  def draws
+    draws = 0
+    gt = GameTeam.where(team_id: self.id)
+
+    for g in gt do
+      game = Game.find(g.game_id)
+      draws += 1 if game.result == 'draw'
+    end
+
+    wins
+  end
+
+  # TODO: cleanup
+  def losses
+    losses = 0
+    gt = GameTeam.where(team_id: self.id)
+
+    for g in gt do
+      opp_side = g.side == 'home' ? 'away' : 'home'
+      game = Game.find(g.game_id)
+      losses += 1 if game.result == opp_side
+    end
+
+    losses
   end
 end
