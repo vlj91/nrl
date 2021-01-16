79fdac288b2a4f73cc1d61a2424e085cbb4374b5
Vaughan Jones <vlj7491@gmail.com>
2021-01-17 10:34:55 +1100

    clean up GameScraperJob

--- app/jobs/game_scraper_job.rb
+++ app/jobs/game_scraper_job.rb
@@ -7,35 +7,31 @@ class GameScraperJob < ApplicationJob
     (1..23).each do |round|
       doc = page_data("#{DRAW_BASE_URL}&round=#{round}", "div#vue-draw")
       doc['fixtures'].each do |fixture|
-        if fixture['roundTitle'] == 'GrandFinal'
-          logger.info "grand final: #{fixture}"
-        else
-          logger.info "home team nickname: #{fixture['homeTeam']['nickName']}"
-          home_team = Team.find_by(name: fixture['homeTeam']['nickName'])
-          logger.info "away team nickname: #{fixture['awayTeam']['nickName']}"
-          away_team = Team.find_by(name: fixture['awayTeam']['nickName'])
+        next if fixture['roundTitle'] == 'GrandFinal'
+        next unless fixture['matchState'] == 'FullTime'
 
-          next unless fixture['matchState'] == 'FullTime'
+        home_team = Team.find_by(name: fixture['homeTeam']['nickName'])
+        away_team = Team.find_by(name: fixture['awayTeam']['nickName'])
 
-          game = Game.find_or_create_by({
-            round: round,
-            title: "#{home_team.slug}-v-#{away_team.slug}"
-          })
+        game = Game.find_or_create_by({
+          round: round,
+          title: "#{home_team.slug}-v-#{away_team.slug}"
+        })
 
-          game_home_team = GameTeam.find_or_create_by({
-            game_id: game.id,
-            team_id: home_team.id,
-            side: 'home',
-            score: fixture['homeTeam']['score']
-          })
+        # create home team GameTeam
+        GameTeam.find_or_create_by({
+          game_id: game.id,
+          team_id: home_team.id,
+          side: 'home',
+          score: fixture['homeTeam']['score']
+        })
 
-          game_away_team = GameTeam.find_or_create_by({
-            game_id: game.id,
-            team_id: away_team.id,
-            side: 'away',
-            score: fixture['awayTeam']['score']
-          })
-        end
+        GameTeam.find_or_create_by({
+          game_id: game.id,
+          team_id: away_team.id,
+          side: 'away',
+          score: fixture['awayTeam']['score']
+        })
       end
     end
   end
