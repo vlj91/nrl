2630b87b2ca8cdb28a216ec19b93533c43d4331d
Vaughan Jones <vlj7491@gmail.com>
2021-01-17 10:45:32 +1100

    scaffold Games controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,10 @@
+class GamesController < ApplicationController
+  def show
+  end
+
+  def list
+  end
+
+  def predict
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'games/show'
+  get 'games/list'
+  get 'games/predict'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
