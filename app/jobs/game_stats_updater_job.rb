1e69f84e16e2edcd660b15462491822ae2a1dabc
Vaughan Jones <vlj7491@gmail.com>
2021-01-31 10:09:36 +1100

    add team total points for each game

--- app/jobs/game_stats_updater_job.rb
+++ app/jobs/game_stats_updater_job.rb
@@ -17,12 +17,44 @@ class GameStatsUpdaterJob < ApplicationJob
     stat.save!
   end
 
+  def update_total_points!(game, side)
+    game_team = GameTeam.find_by({
+      game_id: game.id,
+      side: side
+    })
+
+    tries = GameStat.find_by({
+      name: 'tries',
+      game_id: game.id,
+      team_id: game_team.team_id
+    }).value
+
+    goals = GameStat.find_by({
+      name: 'goals',
+      game_id: game.id,
+      team_id: game_team.team_id
+    }).value
+
+    points = (tries * 4) + (goals * 2)
+
+    stat = GameStat.find_or_create_by({
+      name: 'points',
+      game_id: game.id,
+      team_id: game_team.team_id
+    })
+
+    stat.value = points
+    stat.save!
+  end
+
   def perform(*args)
     for game in Game.all do
       update_team_stat!(game, 'home', 'tries', 'Try')
       update_team_stat!(game, 'away', 'tries', 'Try')
       update_team_stat!(game, 'home', 'goals', 'Goal')
       update_team_stat!(game, 'away', 'goals', 'Goal')
+      update_total_points!(game, 'home')
+      update_total_points!(game, 'away')
     end
   end
 end
