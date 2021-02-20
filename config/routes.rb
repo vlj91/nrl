d5a745f8fa1d5eab5e30ea765c704712bbfc51b7
Vaughan Jones <vlj7491@gmail.com>
2021-02-20 11:33:28 +1100

    routes are dealt with by resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
   resources :games, :players, :teams, :predictions
-  get "/games", to: "games#list"
-  get "/players", to: "players#list"
-  get "/teams", to: "teams#list"
-  get "/predictions/:id", to: "predictions#get"
 end
