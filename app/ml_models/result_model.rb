dd6c2d23aa8143951c8e13e7f6b4377e04059c7e
Vaughan Jones <vlj7491@gmail.com>
2021-02-21 10:49:50 +1100

    clean up the model comments

--- app/ml_models/result_model.rb
+++ app/ml_models/result_model.rb
@@ -33,38 +33,71 @@ class ResultModel < Eps::Base
     away = GameTeam.find_by(game_id: game.id, side: 'away')
 
     {
+      # avg win margin
       'home_team_avg_win_margin': TeamStat.find_by(team_id: home.team_id, name: 'avg_win_margin').value,
       'away_team_avg_win_margin': TeamStat.find_by(team_id: away.team_id, name: 'avg_win_margin').value,
+
+      # avg loss margin
       'home_team_avg_loss_margin': TeamStat.find_by(team_id: home.team_id, name: 'avg_loss_margin').value,
       'away_team_avg_loss_margin': TeamStat.find_by(team_id: away.team_id, name: 'avg_loss_margin').value,
+
+      # avg goals per game
       'home_team_avg_goals_per_game': TeamStat.find_by(team_id: home.team_id, name: 'avg_goals_per_game').value,
       'away_team_avg_goals_per_game': TeamStat.find_by(team_id: away.team_id, name: 'avg_goals_per_game').value,
+
+      # avg goals missed per game
       'home_team_avg_goals_missed_per_game': TeamStat.find_by(team_id: home.team_id, name: 'avg_goals_missed_per_game').value,
       'away_team_avg_goals_missed_per_game': TeamStat.find_by(team_id: away.team_id, name: 'avg_goals_missed_per_game').value,
+
+      # avg tries per game
       'home_team_avg_tries_per_game': TeamStat.find_by(team_id: home.team_id, name: 'avg_tries_per_game').value,
       'away_team_avg_tries_per_game': TeamStat.find_by(team_id: away.team_id, name: 'avg_tries_per_game').value,
+
+      # avg errors per game
       'home_team_avg_errors_per_game': TeamStat.find_by(team_id: home.team_id, name: 'avg_errors_per_game').value,
       'away_team_avg_errors_per_game': TeamStat.find_by(team_id: away.team_id, name: 'avg_errors_per_game').value,
+
+      # avg line breaks per game
       'home_team_avg_line_breaks_per_game': TeamStat.find_by(team_id: home.team_id, name: 'avg_line_breaks_per_game').value,
       'away_team_avg_line_breaks_per_game': TeamStat.find_by(team_id: away.team_id, name: 'avg_line_breaks_per_game').value,
+
+      # avg penalties per game
       'home_team_avg_penalties_per_game': TeamStat.find_by(team_id: home.team_id, name: 'avg_penalties_per_game').value,
       'away_team_avg_penalties_per_game': TeamStat.find_by(team_id: away.team_id, name: 'avg_penalties_per_game').value,
+
+      # avg points per game
       'home_team_avg_points_per_game': TeamStat.find_by(team_id: home.team_id, name: 'avg_points_per_game').value,
       'away_team_avg_points_per_game': TeamStat.find_by(team_id: away.team_id, name: 'avg_points_per_game').value,
+
+      # avg kick bombs per game
       'home_team_avg_kick_bombs_per_game': TeamStat.find_by(team_id: home.team_id, name: 'avg_kick_bombs_per_game').value,
       'away_team_avg_kick_bombs_per_game': TeamStat.find_by(team_id: away.team_id, name: 'avg_kick_bombs_per_game').value,
+
+      # avg forty twenties per game
       'home_team_avg_forty_twenties_per_game': TeamStat.find_by(team_id: home.team_id, name: 'avg_forty_twenties_per_game').value,
       'away_team_avg_forty_twenties_per_game': TeamStat.find_by(team_id: away.team_id, name: 'avg_forty_twenties_per_game').value,
+
+      # number of wins
       'home_team_wins': Team.find(home.team_id).wins,
       'away_team_wins': Team.find(away.team_id).wins,
+
+      # number of draws
       'home_team_drawn': Team.find(home.team_id).draws,
       'away_team_drawn': Team.find(away.team_id).draws,
+
+      # number of losses
       'home_team_losses': Team.find(home.team_id).losses,
       'away_team_losses': Team.find(away.team_id).losses,
+
+      # number of home game wins
       'home_team_home_game_wins': Team.find(home.team_id).home_game_wins,
       'away_team_home_game_wins': Team.find(away.team_id).home_game_wins,
+
+      # number of away game wins
       'home_team_away_game_wins': Team.find(home.team_id).away_game_wins,
       'away_team_away_game_wins': Team.find(away.team_id).away_game_wins,
+
+      # features about the match not specific to a team
       'result': game.result,
       'month': Time.parse(game.date).strftime("%b"),
       'day': Time.parse(game.date).strftime("%a"),
