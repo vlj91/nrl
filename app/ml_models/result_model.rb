74bcac3b697573d2a934b489bbe218fa1ebe4e21
Vaughan Jones <vlj7491@gmail.com>
2021-02-21 10:50:21 +1100

    increase the model validation size

--- app/ml_models/result_model.rb
+++ app/ml_models/result_model.rb
@@ -13,7 +13,7 @@ class ResultModel < Eps::Base
     # train
     data = games.map { |v| features(v) }
     store = Model.where(key: 'result').first_or_initialize
-    model = Eps::Model.new(data, target: :result, split: {validation_size: 0.50})
+    model = Eps::Model.new(data, target: :result, split: {validation_size: 0.75})
     store.update(data: model.to_pmml)
 
     print model.summary
