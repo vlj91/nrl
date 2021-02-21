ad9163f1a339e8ecbc297dd482ceb3cb4476e8d4
Vaughan Jones <vlj7491@gmail.com>
2021-02-21 11:02:16 +1100

    add more resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :games, :players, :teams, :predictions
+  resources :games, :players, :teams, :predictions, :player_stats, :team_stats, :game_events
 
   get "/prediction_accuracy" => "predictions#accuracy"
 end
