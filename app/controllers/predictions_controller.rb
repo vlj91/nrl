669c37d18fc848504c9642fcaeba3b324917b427
Vaughan Jones <vlj7491@gmail.com>
2021-02-21 11:36:38 +1100

    clean up predictions routes

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,10 +1,12 @@
 class PredictionsController < ApplicationController
+  # /predictions/accuracy
   def accuracy
     accuracy = (ResultModel.new.accuracy[:accuracy] * 100).round(2)
 
     render :json => {"accuracy_percent": accuracy}
   end
 
+  # /predction/:id
   def show
   	game = Game.find(params[:id])
   	@prediction = ResultModel.new.predict(game)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :games, :players, :teams, :predictions, :player_stats, :team_stats, :game_events
-
-  get "/games" => "games#list"
-
-  get "/prediction_accuracy" => "predictions#accuracy"
+  # predictions routes
+  get "/predictions/accuracy" => "predictions#accuracy"
+  get "/prediction/:id" => "predictions#show"
 end
