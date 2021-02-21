d788316cbf6b3cbab5799a00e8bfbbd6e288151d
Vaughan Jones <vlj7491@gmail.com>
2021-02-21 16:30:18 +1100

    add odds endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # predictions routes
   get "/predictions/accuracy" => "predictions#accuracy"
   get "/predictions/probability/:id" => "predictions#probability"
+  get "/predictions/odds/:id" => "predictions#odds"
   get "/prediction/:id" => "predictions#show"
 end
