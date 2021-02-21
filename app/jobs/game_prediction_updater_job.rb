feabb90531484e8a04ef04d16eb5b8571dad6933
Vaughan Jones <vlj7491@gmail.com>
2021-02-21 14:14:39 +1100

    log predicted result

--- app/jobs/game_prediction_updater_job.rb
+++ app/jobs/game_prediction_updater_job.rb
@@ -8,6 +8,7 @@ class GamePredictionUpdaterJob < ApplicationJob
 
     for game in games do
       game.predicted_result = ResultModel.predict(game)
+      logger.info "Game #{game.id} prediction: #{game.predicted_result}"
       game.save!
     end
   end
