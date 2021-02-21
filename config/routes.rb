e30f36e6b9c2e827e8a4a9e767215d14dc3e5941
Vaughan Jones <vlj7491@gmail.com>
2021-02-21 11:16:48 +1100

    add game list route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :games, :players, :teams, :predictions, :player_stats, :team_stats, :game_events
 
+  get "/games" => "games#list"
+
   get "/prediction_accuracy" => "predictions#accuracy"
 end
