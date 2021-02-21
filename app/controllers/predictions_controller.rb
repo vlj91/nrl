6be248d8ad4dcc720781420ec68e3daeef70daae
Vaughan Jones <vlj7491@gmail.com>
2021-02-21 16:27:26 +1100

    add odds endpoint, converts probabilities into odds

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -13,6 +13,17 @@ class PredictionsController < ApplicationController
     render :json => probability
   end
 
+  # /predictions/odds/:id
+  def odds
+    probability = ResultModel.new.probability(Game.find(params[:id]))
+
+    probability[:home] = 1 / (probability[:home] / 100)
+    probability[:draw] = 1 / (probability[:draw] / 100)
+    probability[:away] = 1 / (probability[:away] / 100)
+
+    render :json => probability
+  end
+
   # /predction/:id
   def show
   	game = Game.find(params[:id])
