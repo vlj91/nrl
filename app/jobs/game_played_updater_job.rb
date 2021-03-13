009fc85786dbb8561c6fc96eb5b0f41700d3149c
Vaughan Jones <vlj7491@gmail.com>
2021-03-13 16:47:47 +1100

    current date was wrong

--- app/jobs/game_played_updater_job.rb
+++ app/jobs/game_played_updater_job.rb
@@ -2,13 +2,11 @@ class GamePlayedUpdaterJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    current_date = Date.today.strftime("%Y-%m-%D").to_date
-
     for game in Game.all
       # less than or equal to current date isn't strictly reflective
       # of whether the game was played. we know that we'll only run
       # scrape jobs after all games in a current day have finished
-      if game.date.to_date <= current_date
+      if game.date.to_date <= Date.today
         game.played = true
       else
         game.played = false
