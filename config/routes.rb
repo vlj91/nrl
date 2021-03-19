2cc0bdf5c7fb5fbf39b01f71a92278b7d231db34
Vaughan Jones <vlj7491@gmail.com>
2021-03-19 23:48:30 +1100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
-  # /api/v1/predictions
-  get "/api/v1/predictions/accuracy" => "predictions#accuracy"
-  get "/api/v1/predictions/probability/:id" => "predictions#probability"
-  get "/api/v1/predictions/odds/:id" => "predictions#odds"
-  match "/api/v1/predictions/features" => "predictions#features", :via => [:get]
-  match "/api/v1/predictions/:id" => "predictions#show", :via => [:get]
 
-  # /api/v1/games
-  get "/api/v1/games" => "games#index"
-  match "/api/v1/games/:id" => "games#show", :via => [:get]
-  match "/api/v1/games/:season/:round" => "games#find", :via => [:get]
-
-  # players routes
-  get "/api/v1/players" => "players#index"
-  get "/api/v1/players/:id" => "games#show"
-
-  # teams routes
-  get "/api/v1/teams" => "teams#index"
-  get "/api/v1/teams/:id" => "teams#show"
+  namespace :api do
+    namespace :v1 do
+      get "/predictions/accuracy" => "predictions#accuracy"
+      get "/predictions/probability/:id" => "predictions#probability"
+      get "/predictions/odds/:id" => "predictions#odds"
+      match "/predictions/features" => "predictions#features", :via => [:get]
+      match "/predictions/:id" => "predictions#show", :via => [:get]
+    
+      get "/games" => "games#index"
+      match "/games/:id" => "games#show", :via => [:get]
+      match "/games/:season/:round" => "games#find", :via => [:get]
+    
+      get "/players" => "players#index"
+      get "/players/:id" => "games#show"
+    
+      get "/teams" => "teams#index"
+      get "/teams/:id" => "teams#show"
+    end
+  end
 end
