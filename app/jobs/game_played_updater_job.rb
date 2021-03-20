abb436e9c86792335d3796b7c58f8229c3523703
Vaughan Jones <vlj7491@gmail.com>
2021-03-21 09:31:27 +1100

    log which games were updated

--- app/jobs/game_played_updater_job.rb
+++ app/jobs/game_played_updater_job.rb
@@ -2,17 +2,22 @@ class GamePlayedUpdaterJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    updated_games = []
+
     for game in Game.all
       # less than or equal to current date isn't strictly reflective
       # of whether the game was played. we know that we'll only run
       # scrape jobs after all games in a current day have finished
       if game.kickoff_time < Date.today
         game.played = true
+        updated_games.push(game.id)
       else
         game.played = false
       end
 
       game.save!
     end
+
+    logger.info("Updated games to played=true #{updated_games.join(',')}")
   end
 end
