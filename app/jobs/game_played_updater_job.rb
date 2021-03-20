00879d3b8263ff2f6b76f065a7cbb7cff5b057c2
Vaughan Jones <vlj7491@gmail.com>
2021-03-20 17:41:18 +1100

    use kickoff_time

--- app/jobs/game_played_updater_job.rb
+++ app/jobs/game_played_updater_job.rb
@@ -6,7 +6,7 @@ class GamePlayedUpdaterJob < ApplicationJob
       # less than or equal to current date isn't strictly reflective
       # of whether the game was played. we know that we'll only run
       # scrape jobs after all games in a current day have finished
-      if game.date.to_date < Date.today
+      if game.kickoff_time < Date.today
         game.played = true
       else
         game.played = false
