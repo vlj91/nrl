6d5d0aad562d70ebe832d140e5d7a640c0c5e90d
Vaughan Jones <vlj7491@gmail.com>
2021-03-20 19:04:37 +1100

    remove ordering from games

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::GamesController < ApplicationController
   # /api/v1/games
 	def index
-		game = Game.all.order('kickoff_time ASC')
+		game = Game.all
 
 		render json: game
 	end
 
   # /api/v1/games/:season/:round
   def find
-    game = Game.where(round: params[:round], season: params[:season]).order('kickoff_time ASC')
+    game = Game.where(round: params[:round], season: params[:season])
 
     render json: game
   end
