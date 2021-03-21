3119d2bce6f233c5d41dfcdb37b13e849df9a693
Vaughan Jones <vlj7491@gmail.com>
2021-03-21 13:52:41 +1100

    clean game prediction updater

--- app/jobs/game_prediction_updater_job.rb
+++ app/jobs/game_prediction_updater_job.rb
@@ -2,13 +2,11 @@ class GamePredictionUpdaterJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    games ||= Game.all.where(predicted_result: nil)
-    # ensure the model is built
+    games = Game.all
     ResultModel.build
 
     for game in games do
       game.predicted_result = ResultModel.predict(game)
-      logger.info "Game #{game.id} prediction: #{game.predicted_result}"
       game.save!
     end
   end
