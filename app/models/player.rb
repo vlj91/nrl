cd872d42c5d533db0cc4a8b10505d4c893d8e062
Vaughan Jones <vlj7491@gmail.com>
2021-04-05 10:22:38 +1000

    start adding things to player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,24 @@
 class Player < ApplicationRecord
   has_many :game_events
+  has_many :player_stats
+
+  def tries
+    game_events.where(event_type: 'Try')
+  end
+
+  def penalties
+    game_events.where(event_type: 'Penalty')
+  end
+
+  def goals
+    game_events.where(event_type: 'Goal')
+  end
+
+  def average_try_minute
+    player_stats.find_by(name: 'average_try_minute').value
+  end
+
+  def average_tries_per_game
+    player_stats.find_by(name: 'average_tries_per_game').value
+  end
 end
