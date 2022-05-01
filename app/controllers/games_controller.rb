109782879a3138ff3fb96f8a6af4e4da5cf63fae
Vaughan Jones <vlj7491@gmail.com>
2022-05-01 18:14:44 +1000

    move games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,8 @@
 class GamesController < ApplicationController
   def index
     @games = Game.all.order(kickoff_time: :desc)
-    @games = @games.where(season: params[:season]) if params[:season].present?
+    @games = @games.where(season: params[:season]) if params[:season]
+    @games = @games.where(round: params[:round]) if params[:round]
 
     respond_to do |format|
       format.json { render json: @games }
@@ -14,5 +15,7 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+
+    render json: @game
   end
 end
