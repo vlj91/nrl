ed98a2392c3e79be00ecc751a2c7805ec4b24513
Vaughan Jones <vlj7491@gmail.com>
2022-10-31 16:25:30 +1100

    .

--- app/models/game_event.rb
+++ app/models/game_event.rb
@@ -28,4 +28,5 @@
 class GameEvent < ApplicationRecord
   belongs_to :game
   belongs_to :team
+  belongs_to :player
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'game_events/show'
   get 'stats/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
